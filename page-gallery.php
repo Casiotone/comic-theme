40e4e8a3030b22dba6a562bbd83cfdc38f66d33c
Joceline Colvert <jocelinecolvert@gmail.com>
2019-02-24 15:43:17 +0000

    adds custom page template files

---
+++ page-gallery.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for gallery
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Comic-theme
+ */
+
+get_header();
+?>
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+
+<?php get_footer(); ?>
