6ee059853cc416ba4fcf0bedc8b134f34cf30ec1
Kieran Denshi <kierandenshi@mac.com>
2019-03-05 11:56:13 +0000

    use the_content() directly

--- page-contact.php
+++ page-contact.php
@@ -1,28 +1,15 @@
-<?php
-/**
- * The template for contact
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Comic-theme
- */
-
-get_header();
-?>
+<?php get_header(); ?>
 
 		<?php
 		while ( have_posts() ) :
+			
+			
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			the_content();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
 
 		endwhile; // End of the loop.
 		?>
-
 
 <?php get_footer(); ?>
